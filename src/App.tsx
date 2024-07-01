e1587bb40b50ee99eaf48ec912dc9960e987f7f0
Menna Bashir <150310423+MennaBashir@users.noreply.github.com>
2024-07-01 22:02:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import { RecoilRoot } from 'recoil';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import { useTranslation } from 'react-i18next';
 const App: React.FC = () => {
-
   const { t, i18n } = useTranslation();
-  const [active1, setActive1] = useState(true);
-  const [active2, setActive2] = useState(false);
+  const [active1, setActive1] = useState<string>('');
   const direction = i18n.dir(i18n.language);
-  const ChangeLanguage = (lang: string) => {
-    i18n.changeLanguage(lang);
-    setActive1(!active1);
-    setActive2(!active2)
+  const ChangeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setActive1(event.target.value);
   }
+  useEffect(() => {
+    i18n.changeLanguage(active1);
+  }
+    , [active1])
   return (
     <RecoilRoot>
       <div className="container" dir={direction}>
-        <div className="btn">
-          <button className={active1 ? "ar active" : "ar"} onClick={() => ChangeLanguage("ar")}>Ar</button>
-          <button className={active2 ? "en active" : "en"} onClick={() => ChangeLanguage("en")}>En</button>
+        <div className="checklang">
+            <select name="lang" value={active1} onChange={ChangeLanguage}>
+              <option value="en" >EN</option>
+              <option value="ar">AR</option>
+            </select>
         </div>
         <div className="txt">
           <h2>{t('Key_1')}</h2>
